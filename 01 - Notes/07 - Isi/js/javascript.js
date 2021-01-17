630ee8653c3bf872fb0deab0da45c0a5d0c07447
Antoine Chagnon Michaud <achagnonmichaud@cvm.qc.ca>
2021-01-17 14:12:02 -0500

    first commit

---
+++ 01 - Notes/07 - Isi/js/javascript.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+	document.getElementById("memo-input").onkeyup = function(e) {
+		if (e.key == 'Enter') {
+
+		}
+	}
+}
---
+++ Lab - 01 - Fruit ninja/Étudiants/js/javascript.js
@@ -0,0 +1,2 @@
+// Petite note : le noeud "game-over" est existant dans le HTML, mais en display "none". Lorsque la partie se termine
+// vous pouvez l'afficher et cacher le noeud "game"
