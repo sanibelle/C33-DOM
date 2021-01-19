fb43a0ceb1832fc4bb6258d99283ed349901ca1c
Antoine Chagnon Michaud <achagnonmichaud@cvm.qc.ca>
2021-01-19 11:50:08 -0500

    07 done

--- 01 - Notes/07 - Isi/js/javascript.js
+++ 01 - Notes/07 - Isi/js/javascript.js
@@ -1,7 +1,30 @@
 window.onload = function() {
 	document.getElementById("memo-input").onkeyup = function(e) {
 		if (e.key == 'Enter') {
-			console.log(this.value)
+			let node = document.createElement('div') // anciennement appelé nouvelElement
+			let textContent = document.createTextNode(this.value)// anciennement appelé textNode
+			node.appendChild(textContent)
+			node.setAttribute("class", "memo-entry")
+			
+			document.getElementById('memo-list').appendChild(node)
+			this.value = ""
+
+			
+			// fonctionne
+			node.onclick = () => {
+				node.remove()
+			}
+			
+			// Fonctionne aussi
+			// node.onclick = function() {
+			// 	node.remove()
+			// 	// ou this.remove()
+			// }
+			
+			// XXXXXXX Supprime le input
+			// node.onclick = () => {
+			// 	this.remove()
+			// }
 		}
 	}
 }
